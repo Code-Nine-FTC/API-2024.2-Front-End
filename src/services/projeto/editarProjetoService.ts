285fb1a471a738e6db09e2ae06e7fd200b615a65
DfMaciel <104574671+DfMaciel@users.noreply.github.com>
2024-09-26 18:51:25 -0300

    Refactor: Funções de edição, exclusão e visualização do projeto adicionadas a arquivos próprios

---
+++ src/services/projeto/editarProjetoService.ts
@@ -0,0 +1,28 @@
+import api from "../api"
+import { getToken } from "../auth"
+import { AxiosError } from 'axios';
+
+const EditarProjetoService = async (projetoDados: FormData, id: number) => {
+    try {
+        console.log(projetoDados);
+        const resposta = await api.put(`/projeto/atualizar/${id}`, projetoDados, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                Authorization: `Bearer ${getToken()} `
+            }
+        });
+
+        if (resposta.status === 200) {
+            console.log('Projeto cadastrado com sucesso', resposta.data);
+            return { status: resposta.status, data: resposta.data };
+        } else {
+            return { status: resposta.status, message: resposta.data };
+        }
+    } catch (error) {
+        let errorMessage = (error as AxiosError).response?.data as any;
+        // errorMessage = errorMessage? || 'Erro ao realizar o cadastro. Por favor, tente novamente mais tarde.';
+        throw new Error(errorMessage);
+    }
+}
+
+export default EditarProjetoService;
--- src/services/projeto/excluirProjetoService.ts
+++ src/services/projeto/excluirProjetoService.ts
@@ -1,23 +1,23 @@
 import api from "../api";
 import { getToken } from "../auth";
 
-export default async function ExcluirArquivo (id: number): Promise<any> {
+const ExcluirProjeto = async (id: number): Promise<any> => {
     try {
-        const resposta = await api.delete(`/documento/excluir/${id}`,
-        {
-            headers: {
-                Authorization: `Bearer ${getToken()}`
-            }
-        }
-        );
+        const resposta = await api.delete(`/projeto/deletar/${id}`, {
+          headers: {
+            Authorization: `Bearer ${getToken()}`,
+          },
+        });
         if (resposta.status === 200) {
-            console.log('Arquivo excluído com sucesso', resposta.data);
+            console.log("Projeto deletado com sucesso!");
             return { status: resposta.status, data: resposta.data };
         } else {
             return { status: resposta.status, message: resposta.data };
         }
-    } catch (error) {
+      } catch (error) {
         console.error('Erro ao excluir o arquivo', error);
         throw new Error('Erro ao excluir o arquivo. Tente novamente mais tarde');
-    	}
+      }
 }
+
+export default ExcluirProjeto;
