272e82b127521f29f249f1c7dc8e612a535784c7
Renato-Cruz-Jr <142221532+Renato-Cruz-Jr@users.noreply.github.com>
2024-09-27 10:47:33 -0300

    feat: Proteção de rotas do Adm no Frontend implementada

--- src/rotas/rotas.tsx
+++ src/rotas/rotas.tsx
@@ -1,18 +1,44 @@
-import { FC } from "react";
-import { Route, Routes } from 'react-router-dom';
+import { FC, useContext } from "react";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
 import HomeBuscar from "../pages/homePage";
 import CriarProjeto from "../pages/criarProjeto";
 import Login from "../pages/loginPage";
 import AlteraDeletaVisualiza from "../pages/visualizarProjetos";
-const Rotas: FC = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<HomeBuscar />} />
-            <Route path="/adicionarprojeto" element={<CriarProjeto />} />
-            <Route path="/projeto/visualizar/:id" element={<AlteraDeletaVisualiza/>} />
-            <Route path="/login" element={<Login />} />
-        </Routes>
-    );
-}
+import { AuthContext } from '../services/context';
 
-export default Rotas;
+const Rotas: FC = () => {
+    const { isAutenticado, nivelAcesso } = useContext(AuthContext);
+
+    let rotas;
+
+    if (isAutenticado) {
+        if (nivelAcesso === 'administrador') {
+          rotas = (
+            <>
+                {/* Apenas administradores podem adicionar projetos */}
+                <Route path="/adicionarprojeto" element={<CriarProjeto />} />
+            </>
+        );  
+        } else {
+            return <Route path="*" element={<Navigate to="/" replace />} />;
+        }
+    }
+    return (
+        <BrowserRouter>
+          <Routes>
+            {/* Rotas abertas para todos */}
+            <Route path="/" element={<HomeBuscar />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/projeto/visualizar/:id" element={<AlteraDeletaVisualiza/>} />
+    
+            {/* Rotas condicionadas com base no nível de acesso */}
+            {rotas}
+    
+            {/* Redireciona qualquer rota inválida para a página inicial */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      );
+    }
+    
+    export default Rotas;
--- src/services/context.tsx
+++ src/services/context.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 export const AuthContext = React.createContext({
   isAutenticado: false,
   token: '',
+  nivelAcesso: '',
   setAutenticado: (status: boolean) => {},
   setToken: (token: string) => {},
+  setNivelAcesso: (level: string) => {},
 });
