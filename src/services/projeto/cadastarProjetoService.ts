1776eb08b2bcb8bec5431c021b5aa44c101f64a7
Pedro Martins de Oliveira <142221456+OliveiraPedro09@users.noreply.github.com>
2024-09-27 10:22:08 -0300

    Refactor: Tirando os console log do Service
    Tirei os console.log do cadastrarProjetoService

--- src/services/projeto/cadastarProjetoService.ts
+++ src/services/projeto/cadastarProjetoService.ts
@@ -5,14 +5,11 @@ import { getToken } from '../auth';
 
 export default async function CadastrarProjetoFunction (projeto: CadastrarProjeto): Promise<any> {
     try {
-        console.log(getToken());
         const resposta = await api.post('/projeto/cadastrar', projeto, {
             headers: {
                 Authorization: `Bearer ${getToken()} `
             }
         });
-
-        console.log(resposta);
 
         if (resposta.status === 201) {
             console.log('Projeto cadastrado com sucesso', resposta.data);
@@ -20,6 +17,7 @@ export default async function CadastrarProjetoFunction (projeto: CadastrarProjet
         } else {
             return { status: resposta.status, message: resposta.data };
         }
+        
     } catch (error) {
         let errorMessage = (error as AxiosError).response?.data as any;
         // errorMessage = errorMessage? || 'Erro ao realizar o cadastro. Por favor, tente novamente mais tarde.';
