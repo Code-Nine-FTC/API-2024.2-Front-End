298d1f68ed9a400704269077656ef6af6110ba0e
Renato-Cruz-Jr <142221532+Renato-Cruz-Jr@users.noreply.github.com>
2024-10-01 12:06:07 -0300

    feat: criei a página do Dashboard e a conectei com o componente formulário do Dashboard.

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardFormComponent from '../component/Dashboard/DashformFormComponent';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <DashboardFormComponent />
+    </div>
+  );
+};
+
+export default Dashboard;
