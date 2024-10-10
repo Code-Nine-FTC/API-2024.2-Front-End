1345aa5ee4ed024762232c5fc1e9b47d83f54835
GustavoCastilhoLucena <gustavocastilho245@gmail.com>
2024-10-10 20:29:39 -0300

    feat: adicionei a capacidade dos dados serem chamados por fetch no grafico

--- src/component/Dashboard/charts/bar.tsx
+++ src/component/Dashboard/charts/bar.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react"
 import { Bar } from "react-chartjs-2"
 import { 
     Chart as ChartJS, 
@@ -6,9 +7,21 @@ import {
     Title,
     Tooltip,
     Legend,
-    BarElement
+    BarElement,
+    ChartData,
+    ChartOptions
 } from "chart.js"
 import {BarChartData} from "./testData/fakeData"
+
+interface FetchBarGraphData {
+    month: string
+    value: number
+}
+
+interface BarGraphProps {
+    options: ChartOptions<'bar'>
+    data2: FetchBarGraphData[]
+}
 
 ChartJS.register(
     CategoryScale, 
@@ -23,8 +36,30 @@ const options = {}
 
 const data = {}
 
-const BarGraph = () => {
-    return <Bar options={options} data={BarChartData}/>
+const BarGraph: React.FC<BarGraphProps> = ({ options }) => {
+    const [data2, setData2] = useState<FetchBarGraphData[]>([])
+
+    useEffect(() => {
+        fetch('/data.json')
+            .then(response => response.json())
+            .then(data => setData2(data))
+            .catch(error => console.error('Erro ao buscar os dados: ', error))
+    }, [])
+
+    const data = {
+        labels: data2.map(row => row.month),
+        datasets: [
+            {
+                label: "Gastos",
+                data: data2.map(row => row.value),
+                backgroundColor: ["rgba(54, 162, 235, 1)"],
+                borderColor: ["rgba(54, 162, 235, 1)"],
+                borderWidth: 1
+            }
+        ]
+    }
+    
+    return <Bar options={options} data={data} />
 }
 
 export default BarGraph
