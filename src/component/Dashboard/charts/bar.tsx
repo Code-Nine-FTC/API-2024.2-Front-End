7812ebf87ccd91e7773134949825491357107a3f
JonasOliver <jonaasmigueldeoliveira@gmail.com>
2024-10-19 13:17:05 -0300

    Fix: Atualização no do bar do dashboard, para requisição de dados

--- src/component/Dashboard/charts/bar.tsx
+++ src/component/Dashboard/charts/bar.tsx
@@ -1,51 +1,30 @@
-import React, { useEffect, useState } from "react"
-import { Bar } from "react-chartjs-2"
-import { 
-    Chart as ChartJS, 
-    CategoryScale, 
-    LinearScale, 
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
     Title,
     Tooltip,
     Legend,
     BarElement,
     ChartData,
-    ChartOptions
-} from "chart.js"
-import {BarChartData} from "./testData/fakeData"
+    ChartOptions,
+} from "chart.js";
 
 interface FetchBarGraphData {
-    month: string
-    value: number
+    month: string;
+    value: number;
 }
 
 interface BarGraphProps {
-    options: ChartOptions<'bar'>
-    data2: FetchBarGraphData[]
+    options: ChartOptions<'bar'>;
+    data2: FetchBarGraphData[];
 }
 
-ChartJS.register(
-    CategoryScale, 
-    LinearScale, 
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-)
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const options = {}
-
-const data = {}
-
-const BarGraph: React.FC<BarGraphProps> = ({ options }) => {
-    const [data2, setData2] = useState<FetchBarGraphData[]>([])
-
-    useEffect(() => {
-        fetch('/data.json')
-            .then(response => response.json())
-            .then(data => setData2(data))
-            .catch(error => console.error('Erro ao buscar os dados: ', error))
-    }, [])
-
+const BarGraph: React.FC<BarGraphProps> = ({ options, data2 }) => {
     const data = {
         labels: data2.map(row => row.month),
         datasets: [
@@ -54,12 +33,12 @@ const BarGraph: React.FC<BarGraphProps> = ({ options }) => {
                 data: data2.map(row => row.value),
                 backgroundColor: ["rgba(54, 162, 235, 1)"],
                 borderColor: ["rgba(54, 162, 235, 1)"],
-                borderWidth: 1
-            }
-        ]
-    }
-    
-    return <Bar options={options} data={data} />
-}
+                borderWidth: 1,
+            },
+        ],
+    };
 
-export default BarGraph
+    return <Bar options={options} data={data} />;
+};
+
+export default BarGraph;
