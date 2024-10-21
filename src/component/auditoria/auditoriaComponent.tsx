4c18a0266f97a07d062c3e2b4e912465febb2ed9
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-10-21 08:24:37 -0300

    feat: criado componente da pagina auditoria

---
+++ src/component/auditoria/auditoriaComponent.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const AuditoriaComponent = () => {
+    return(
+        <h1>
+            Teste
+        </h1>
+    )
+}
+
+export default AuditoriaComponent
