eb85f08cebf619e537cc10432381a1dd088b54ea
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-10-21 08:25:31 -0300

    feat: criada pagina auditoria

---
+++ src/pages/auditoria.tsx
@@ -0,0 +1,11 @@
+import AuditoriaComponent from "../component/auditoria/auditoriaComponent";
+
+const Auditoria = () => {
+    return(
+        <div>
+            <AuditoriaComponent/>
+        </div>
+    )
+}
+
+export default Auditoria
