2ad02bcd98cce33537dc80711fc06227e9807118
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-10-22 11:56:39 -0300

    feat: criado componente de auditoria

--- src/component/auditoria/auditoriaComponent.tsx
+++ src/component/auditoria/auditoriaComponent.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 const AuditoriaComponent = () => {
+    const [dados, setDados] = useState([])
+    useEffect(()=>{
+
+    })
+
     return(
         <h1>
             Teste
