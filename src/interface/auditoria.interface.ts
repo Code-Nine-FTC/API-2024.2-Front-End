6d8e2e8c74959b7c823348aae146d18f71a8f90f
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-10-25 09:23:45 -0300

    feat: adicionado usuario na interface

--- src/interface/auditoria.interface.ts
+++ src/interface/auditoria.interface.ts
@@ -3,6 +3,7 @@ import { MudancaProjeto } from "./projeto.interface";
 export interface Mudanca{
     id: string,
     evento: string,
+    usuario?: string
     data: Date,
     projeto: MudancaProjeto
 }
