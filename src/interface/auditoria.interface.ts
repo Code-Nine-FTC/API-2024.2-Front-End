22a0ef867eb0a8519dcc819ff7159959e359857d
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-10-30 09:42:32 -0300

    feat: nova interface para o novo modelo de auditoria

--- src/interface/auditoria.interface.ts
+++ src/interface/auditoria.interface.ts
@@ -1,27 +1,57 @@
-import { VisualizarDocumento } from "./documento.interface";
-import { MudancaProjeto } from "./projeto.interface";
+import { VisualizarDocumento } from "./documento.interface"
+import { MudancaProjeto } from "./projeto.interface"
 
-export interface Mudanca{
-    id: string,
-    evento: string,
+export interface Mudanca {
+    id: string
+    evento: string
     usuario?: string
-    data: Date,
+    data: Date
     projeto: MudancaProjeto
 }
 
 export interface AuditorarProjeto {
-    id?: number;
-    titulo?: string;
-    referencia?: string;
-    contratante?: string;
-    objeto?: string;
-    descricao?: string;
-    nomeCoordenador?: string;
-    dataInicio?: Date;
-    dataTermino?: Date;
-    valor?: number;
-    integrantes?: string;
-    status?: string;
-    links?: string;
-    documentos?: VisualizarDocumento[];
+    id?: number
+    titulo?: string
+    referencia?: string
+    contratante?: string
+    objeto?: string
+    descricao?: string
+    nomeCoordenador?: string
+    dataInicio?: Date
+    dataTermino?: Date
+    valor?: number
+    integrantes?: string
+    status?: string
+    links?: string
+    documentos?: VisualizarDocumento[]
+}
+
+export interface Auditoria {
+    id: number
+    projetoId: number
+    nomeCoordenador?: string
+    tituloAntigo?: string
+    referencia: string
+    contratanteAntigo?: string
+    descricaoAntiga?: string
+    valorAntigo?: number
+    dataInicioAntiga?: Date
+    dataTerminoAntiga?: Date
+    statusAntigo?: string
+    integrantesAntigos?: string
+    objetivoAntigo?: string
+    linksAntigos?: string
+    tituloNovo?: string
+    contratanteNovo?: string
+    descricaoNovo?: string
+    valorNovo?: number
+    dataInicioNovo?: Date
+    dataTerminoNovo?: Date
+    statusNovo?: string
+    integrantesNovo?: string
+    objetivoNovo?: string
+    linksNovo?: string
+    arquivoOriginal1?: Blob
+    arquivoOriginal2?: Blob
+    dataAlteracao?: Date
 }
