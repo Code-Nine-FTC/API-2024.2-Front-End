6fa04890f6d8f6bdc5cf463c08355e5cbe665202
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-11-13 08:31:53 -0300

    feat: dropdown modificado para paginas certas

--- src/component/cadastros/cadastrosDropdown/CadastrosDropdownComponent.tsx
+++ src/component/cadastros/cadastrosDropdown/CadastrosDropdownComponent.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Dropdown, Nav } from 'react-bootstrap';
 import styles from './CadastrosDropdownComponent.module.css';
+import { useNavigate } from 'react-router-dom';
 
 interface CadastrosDropdownComponentProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const CadastrosDropdownComponent: React.FC<CadastrosDropdownComponentProps> = ({ className }) => {
+  const navigate = useNavigate();
+
   return (
     <Nav.Item className={`${styles.dropdownHover} ${className}`}>
       <Dropdown show={true}>
@@ -18,8 +21,10 @@ const CadastrosDropdownComponent: React.FC<CadastrosDropdownComponentProps> = ({
         </Dropdown.Toggle>
 
         <Dropdown.Menu className={`${styles.dropdownMenu}`}>
-          <Dropdown.Item href="#/action-1" className={styles.dropdownItem}>Gastos</Dropdown.Item>
-          <Dropdown.Item href="#/action-2" className={styles.dropdownItem}>Receitas</Dropdown.Item>
+          <Dropdown.Item href="#/action-1" className={styles.dropdownItem}>Parceiros</Dropdown.Item>
+          <Dropdown.Item href="#/action-2" className={styles.dropdownItem}>Bolsistas</Dropdown.Item>
+          <Dropdown.Item onClick={() => navigate('/cadastroMaterial')} className={styles.dropdownItem}>Materiais</Dropdown.Item>
+          <Dropdown.Item href="#/action-2" className={styles.dropdownItem}>Classificação de Demandas</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </Nav.Item>
