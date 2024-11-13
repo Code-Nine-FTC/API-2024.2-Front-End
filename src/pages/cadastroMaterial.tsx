f868bec1be8697d25e1334ede2487dcf64b5aa16
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-11-13 08:33:22 -0300

    feat: pagina de cadastro de materiais criada

---
+++ src/pages/cadastroMaterial.tsx
@@ -0,0 +1,11 @@
+import CadastroMaterialComponent from "../component/cadastros/cadastroMaterial/CadastroMaterialComponent";
+
+const CadastroMaterial = () => {
+    return (
+        <div>
+            <CadastroMaterialComponent/>
+        </div>
+    )
+}
+
+export default CadastroMaterial
