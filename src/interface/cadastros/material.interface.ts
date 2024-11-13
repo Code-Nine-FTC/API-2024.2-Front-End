9e4a97aea3aa864dc9b9c41a860f90a52e2387f7
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-11-13 08:32:50 -0300

    feat: interface de cadastro de materiais criada

---
+++ src/interface/cadastros/material.interface.ts
@@ -0,0 +1,8 @@
+export interface MaterialCadastro {
+    nome: string
+    valor: number
+    fornecedor: string
+    fornecedorEmail: string
+    fornecedorTelefone: string
+    statusUtilizacao: string
+}
