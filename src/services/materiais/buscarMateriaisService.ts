43376f5b25d9eb1e5c2dd9ae6e78278272c35970
DfMaciel <104574671+DfMaciel@users.noreply.github.com>
2024-11-16 14:34:40 -0300

    Feat: Função de busca de materiais criada

---
+++ src/services/materiais/buscarMateriaisService.ts
@@ -0,0 +1,25 @@
+import api from "../api"
+import { getToken } from "../auth"
+import { AxiosError } from "axios"
+
+export default async function buscarMateriaisService () {
+    try {
+        const resposta = await api.get('/material/listar', {
+            headers: {
+                Authorization: `Bearer ${getToken()}`
+            }
+        })
+        if (resposta.status === 200) {
+            return { status: resposta.status, data: resposta.data };
+        } else {
+            return { status: resposta.status, message: resposta.data || 'Erro ao buscar materiais.' };
+        }
+
+    } catch (error) {
+        const axiosError = error as AxiosError<{ message?: string }>;
+        const errorMessage = axiosError.response?.data?.message || 'Erro ao buscar materiais. Por favor, tente novamente mais tarde.';
+        
+        console.error('Erro ao buscar materiais:', errorMessage);
+        throw new Error(errorMessage);
+    }
+}
