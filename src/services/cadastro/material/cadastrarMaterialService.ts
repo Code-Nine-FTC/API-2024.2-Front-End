4eb86f892f3b4a5615f39beef83057c08468ed4f
Gustavo Castilho <gustavocastilho245@gmail.com>
2024-11-18 08:54:36 -0300

    feat: serviço de cadastro de material criado

---
+++ src/services/cadastro/material/cadastrarMaterialService.ts
@@ -0,0 +1,26 @@
+import { AxiosError } from "axios";
+import { MaterialCadastro } from "../../../interface/cadastros/material.interface";
+import api from "../../api";
+import { getToken } from "../../auth";
+
+export default async function CadastrarMaterialFunction (material: MaterialCadastro): Promise<any> {
+    try {
+        const resposta = await api.post('/material/cadastrar', material, {
+            headers: {
+                Authorization: `Bearer ${getToken()} `
+            }
+        });
+
+        if (resposta.status === 201) {
+            console.log('Material cadastrado com sucesso', resposta.data);
+            return { status: resposta.status, data: resposta.data };
+        } else {
+            return { status: resposta.status, message: resposta.data };
+        }
+        
+    } catch (error) {
+        let errorMessage = (error as AxiosError).response?.data as any;
+        // errorMessage = errorMessage? || 'Erro ao realizar o cadastro. Por favor, tente novamente mais tarde.';
+        throw new Error(errorMessage);
+    }
+} 
